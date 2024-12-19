cf86f3c96ef78a080752079df03ed4df4348966e
sidram403 <siddharthpatil9108@gmail.com>
2024-12-19 15:15:31 +0530

    Footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,22 +18,22 @@ const Footer = () => {
             </div>
             <div>
               <p className="text-lg font-semibold mb-2">Contact Us</p>
-              <h3 className="text-2xl md:text-4xl">WORKLAH! PVT. LTD.</h3>
+              <h3 className="text-2xl md:text-4xl">Attain Consulting Group Pte. Ltd.</h3>
               <div className="flex items-center gap-2 text-xl mt-2">
                 <MapPin className="text-blue-500" />
-                <span>123 Orchard Road, #04-56, Singapore 238888</span>
+                <span>10 ANSON ROAD #10-11 INTERNATIONAL PLAZA SINGAPORE (079903)</span>
               </div>
               <div className="flex items-center gap-2 text-xl mt-2">
                 <Mail className="text-blue-500" />
                 <a href="mailto:support@worklah.in" className="hover:underline">
-                  <p className="text-white"> support@worklah.in </p>
+                  <p className="text-white">admin@worklah.com.sg</p>
                 </a>
               </div>
             </div>
           </div>
 
           {/* Right Section */}
-          <div className="flex flex-col md:flex-row gap-8 md:gap-16">
+          {/* <div className="flex flex-col md:flex-row gap-8 md:gap-16">
             <div className="flex flex-col">
               <h3 className="text-lg font-semibold mb-2">Articles</h3>
               <ul className="text-xl space-y-2">
@@ -49,12 +49,12 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-          </div>
+          </div> */}
         </div>
 
         {/* Bottom Section */}
         <div className="mt-8 pt-4 text-center text-sm text-gray-500">
-          © 2023 WORKLAH! PTE. LTD.
+          © 2023 Attain Consulting Group Pte. Ltd.
         </div>
       </div>
     </footer>
